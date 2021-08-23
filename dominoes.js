0947e77c1b17eb5bb93799a10fdd4f62b4018442
Alex <alexmayolc@gmail.com>
2021-08-23 02:12:38 +0200

    dominoes v2: clean code and fail-proof :v:

--- dominoes.js
+++ dominoes.js
@@ -20,155 +20,117 @@ const getOrientation = (string) => {
     const RIGHT = 'R';
     const LEFT = 'L';
     const STILL = '.';
-    const OUT = 'out';
+    const OUT_OF_BOUNDS = undefined;
 
     let pieces = string.split('');
     let checked = pieces.map((_) => false);
 
-    let finished = false;
     let result = [...pieces];
-    let iteration = [...result];
+    let iteration = [...pieces];
 
-    const leftNeighbour = (index) => {
-        switch (result[index - 1]) {
-            case RIGHT:
-                return RIGHT;
-            case LEFT:
-                return LEFT;
-            case STILL:
-                return STILL;
-            case undefined:
-                return OUT;
+    let changesThisIteration = false;
+    let finished = false;
+
+
+    const modifyBasedOnNeighbours = (index) => {
+        if (result[index - 1] === OUT_OF_BOUNDS) {
+            if (result[index + 1] === LEFT) {
+                changesThisIteration = true
+                checked[index] = true
+                return LEFT
+            }
+
+            if (result[index + 1] === RIGHT) {
+                changesThisIteration = true
+                checked[index] = true
+                return STILL
+            }
+
+            return STILL
         }
-    }
 
-    const rightNeighbour = (index) => {
-        switch (result[index + 1]) {
-            case RIGHT:
-                return RIGHT;
-            case LEFT:
-                return LEFT;
-            case STILL:
-                return STILL;
-            case undefined:
-                return OUT;
+        if (result[index + 1] === OUT_OF_BOUNDS) {
+            if (result[index - 1] === LEFT) {
+                changesThisIteration = true
+                checked[index] = true
+                return STILL
+            }
+
+            if (result[index - 1] === RIGHT) {
+                changesThisIteration = true
+                checked[index] = true
+                return RIGHT
+            }
+
+            return STILL
         }
-    }
 
+        if (result[index - 1] === LEFT) {
+            if (result[index + 1] === LEFT) {
+                changesThisIteration = true
+                checked[index] = true
+                return LEFT
+            }
+
+            if (result[index + 1] === RIGHT) {
+                changesThisIteration = true
+                checked[index] = true
+                return STILL
+            }
+
+            return STILL
+
+        }
+
+        if (result[index - 1] === RIGHT) {
+            if (result[index + 1] === LEFT) {
+                changesThisIteration = true
+                checked[index] = true
+                return STILL
+            }
+
+            if (result[index + 1] === RIGHT || result[index + 1] === STILL) {
+                changesThisIteration = true
+                checked[index] = true
+                return RIGHT
+            }
+
+        }
+
+        if (result[index - 1] === STILL) {
+            if (result[index + 1] === LEFT) {
+                changesThisIteration = true
+                checked[index] = true
+                return LEFT
+            }
+
+            return STILL
+
+        }
+
+    }
 
     while (!finished) {
-        iteration.forEach((piece, index) => {
 
-
-            if (checked[index]) return;
-
-            if (piece !== STILL) {
-                checked[index] = true;
+        result.forEach((piece, index) => {
+            if (checked[index])
                 return;
-            }
-            if (leftNeighbour(index) === OUT && checked[index + 1]) {
-                checked[index] = true;
-                iteration[index] = STILL;
-                return;
+
+            if (piece != STILL) {
+                checked[index] = true
+                changesThisIteration = true
+                return
             }
 
-            if (leftNeighbour(index) === LEFT && rightNeighbour(index) !== LEFT && checked[index + 1]) {
-                checked[index] = true;
-                iteration[index] = STILL;
-                return;
-            }
-
-            if (leftNeighbour(index) === OUT && rightNeighbour(index) === LEFT) {
-                checked[index] = true;
-                iteration[index] = LEFT;
-                return;
-            }
-
-            if (leftNeighbour(index) === OUT && checked[index + 1]) {
-                checked[index] = true;
-                iteration[index] = STILL;
-                return;
-            }
-            if (leftNeighbour(index) === OUT && rightNeighbour(index) === RIGHT) {
-                checked[index] = true;
-                iteration[index] = STILL;
-                return;
-            }
-
-            if (leftNeighbour(index) === RIGHT && rightNeighbour(index) === OUT) {
-                checked[index] = true;
-                iteration[index] = RIGHT;
-                return;
-            }
-
-
-            if (leftNeighbour(index) === LEFT && rightNeighbour(index) === OUT) {
-                checked[index] = true;
-                iteration[index] = STILL;
-                return;
-            }
-            if (leftNeighbour(index) === STILL && rightNeighbour(index) === OUT && checked[index - 2]) {
-                checked[index] = true;
-                iteration[index] = STILL;
-                return;
-            }
-
-            if (leftNeighbour(index) === LEFT && rightNeighbour(index) === OUT && checked[index + 1]) {
-                checked[index] = true;
-                iteration[index] = STILL;
-                return;
-            }
-
-            if (leftNeighbour(index) !== RIGHT && rightNeighbour(index) === LEFT) {
-                checked[index] = true;
-                iteration[index] = LEFT;
-                return;
-            }
-
-            if (leftNeighbour(index) !== RIGHT && rightNeighbour(index) === RIGHT) {
-                checked[index] = true;
-                iteration[index] = STILL;
-                return;
-            }
-
-            if (leftNeighbour(index) === RIGHT && rightNeighbour(index) !== LEFT) {
-                checked[index] = true;
-                iteration[index] = RIGHT;
-                return;
-            }
-
-            if (leftNeighbour(index) === LEFT && rightNeighbour(index) !== LEFT) {
-                checked[index] = false;
-                iteration[index] = STILL;
-                return;
-            }
-
-            if (leftNeighbour(index) === RIGHT && rightNeighbour(index) === LEFT) {
-                checked[index] = true;
-                iteration[index] = STILL;
-                return;
-            }
-
-            if (leftNeighbour(index) === STILL && rightNeighbour(index) === STILL) {
-                checked[index] = false;
-                return;
-            }
-
-            if (leftNeighbour(index) === STILL && rightNeighbour(index) === RIGHT) {
-                checked[index] = false;
-                return;
-            }
-
-            if (leftNeighbour(index) === STILL && rightNeighbour(index) === LEFT) {
-                checked[index] = true;
-                iteration[index] = LEFT;
-                return;
-            }
-
+            iteration[index] = modifyBasedOnNeighbours(index)
 
         })
-        finished = !checked.some(check => check === false);
+
+        if (!changesThisIteration) {
+            finished = true
+        }
         result = [...iteration];
+        changesThisIteration = false
     }
 
     return result.join('');
@@ -180,10 +142,48 @@ const test2 = '.L.R....L';
 const solution2 = 'LL.RRRLLL';
 const test3 = '..R...L.L..';
 const solution3 = '..RR.LLLL..';
-const test4 = '..R...L.L..';
-const solution4 = '..RR.LLLL..';
+const test4 = '.RL..RLRL.RL.RL';
+const solution4 = '.RL..RLRL.RL.RL';
+const test5 = '..L.L.R.L...R.R...LL.R.L';
+const solution5 = 'LLLLL.R.L...RRRR.LLL.R.L';
+const test6 = '...RRLL...L.LLL.RRL..RLLR.RRRL..LL.RRRL.RLLR..RLRLLR.';
+const solution6 = '...RRLLLLLLLLLL.RRL..RLLRRRRRLLLLL.RRRL.RLLRRRRLRLLRR';
+const test7 = 'LR...';
+const solution7 = 'LRRRR';
+const test8 = '....L';
+const solution8 = 'LLLLL';
+const test9 = '..LRLLR...LR...RLLR..LRLR.LR.RLR.';
+const solution9 = 'LLLRLLRR.LLRRRRRLLRRLLRLR.LRRRLRR';
+const test10 = 'R..........L';
+const solution10 = 'RRRRRRLLLLLL';
+const test11 = 'R...........L';
+const solution11 = 'RRRRRR.LLLLLL';
+const test12 = 'R..L..R...L....R.....L.....RLLLRL......RLL...RLL...RR....L.....L';
+const solution12 = 'RRLL..RR.LL....RRR.LLL.....RLLLRL......RLL...RLL...RRRRLLLLLLLLL';
+const test13 = 'R.................';
+const solution13 = 'RRRRRRRRRRRRRRRRRR';
+const test14 = 'L.................';
+const solution14 = 'L.................';
+const test15 = '.................L';
+const solution15 = 'LLLLLLLLLLLLLLLLLL';
+const test16 = '.................R';
+const solution16 = '.................R';
 
+console.time('BENCHMARK')
 console.log(getOrientation(test1) === solution1);
 console.log(getOrientation(test2) === solution2);
 console.log(getOrientation(test3) === solution3);
 console.log(getOrientation(test4) === solution4);
+console.log(getOrientation(test5) === solution5);
+console.log(getOrientation(test6) === solution6);
+console.log(getOrientation(test7) === solution7);
+console.log(getOrientation(test8) === solution8);
+console.log(getOrientation(test9) === solution9);
+console.log(getOrientation(test10) === solution10);
+console.log(getOrientation(test11) === solution11);
+console.log(getOrientation(test12) === solution12);
+console.log(getOrientation(test13) === solution13);
+console.log(getOrientation(test14) === solution14);
+console.log(getOrientation(test15) === solution15);
+console.log(getOrientation(test16) === solution16);
+console.timeEnd('BENCHMARK')
